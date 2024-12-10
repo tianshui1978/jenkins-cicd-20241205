f80f4c7878ebceaf28fc2b2d2f53d857385a2723
tianshui1978 <tianshui1978@sina.com>
2024-12-10 15:33:55 +0800

    aaa

---
+++ pytest-05.py
@@ -0,0 +1,14 @@
+# test_example.py
+
+def add(a, b):
+    """Return the sum of a and b"""
+    return a + b
+
+def test_add1():
+    assert add(2, 3) == 5
+
+def test_add2():
+    assert add(-1, 1) == 0
+
+def test_add3():
+    assert add(-1, -1) == -2
